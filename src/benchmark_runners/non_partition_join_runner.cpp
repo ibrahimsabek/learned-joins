55aeeaebff13d6ce45408cd317e4b874c5c15dd5
Ibrahim Sabek <sabek@mit.edu>
2021-02-15 17:40:44 -0500

    Source code for non-partition join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -411,11 +411,11 @@ void * npj_join_thread(void * param)
     uint32_t nbuckets = (args->relR.num_tuples / BUCKET_SIZE / NUM_THREADS);
 
     if (args->tid == 0) {
-        strcpy(npj_pfun[0].fun_name, "IMV");
-        strcpy(npj_pfun[1].fun_name, "Naive");
+        strcpy(npj_pfun[1].fun_name, "IMV");
+        strcpy(npj_pfun[0].fun_name, "Naive");
 
-        npj_pfun[0].fun_ptr = npj_build_rel_r_partition_imv;
-        npj_pfun[1].fun_ptr = npj_build_rel_r_partition;
+        npj_pfun[1].fun_ptr = npj_build_rel_r_partition_imv;
+        npj_pfun[0].fun_ptr = npj_build_rel_r_partition;
 
         npj_pf_num = 2;
     }
@@ -492,8 +492,8 @@ void * npj_join_thread(void * param)
             }
             //if((fid < npj_pf_num - 1) && (rp < RUN_NUMS - 1)){
                 // clean-up the overflow buffers and hashtable
-                destroy_hashtable(args->ht);
-                free_bucket_buffer(overflowbuf);
+                //destroy_hashtable(args->ht);
+                //free_bucket_buffer(overflowbuf);
             //}
         }
     }
