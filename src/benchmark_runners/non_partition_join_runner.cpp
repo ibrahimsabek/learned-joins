08034c5603b4669d47fa6b343c3acbe0018858e2
Ibrahim Sabek <sabek@mit.edu>
2021-02-16 01:30:45 -0500

    Source code for non-partition join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -810,6 +810,8 @@ void * npj_join_thread(void * param)
             }
         
             if(!((fid == (npj_pf_num - 1)) && (rp == (RUN_NUMS - 1)))){
+            if(args->tid == 0)
+                printf("here \n");
                 if(args->tid == 0)
                     destroy_hashtable(args->ht);
                 BARRIER_ARRIVE(args->barrier, rv);
