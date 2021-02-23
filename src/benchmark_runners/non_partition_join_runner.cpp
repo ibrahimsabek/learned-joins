8c5473ce64634a11123fc0fa049b15269bcde8c1
Ibrahim Sabek <sabek@mit.edu>
2021-02-23 02:57:13 -0500

    Source code for non-partition join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -903,25 +903,25 @@ void sample_and_train_models_threaded(ETHNonPartitionJoinThread<KeyType, Payload
     //                     TRAIN THE MODELS                     //
     //----------------------------------------------------------//
 
-    if(tid==0)
+    if(tid == 0)
     {
         // Stop early if the array is identical
         if (((*(args->rmi->training_sample))[0]).key == ((*(args->rmi->training_sample))[total_sample_count - 1]).key) 
         {
             return;
         }
-/*    
+    
     //printf("key1 %ld, key2 %ld \n", ((sorted_training_sample)[0]).key, ((sorted_training_sample)[total_sample_count - 1]).key);        
     //printf("key1 %ld, key2 %ld \n", ((sorted_training_sample)[0]).key, ((sorted_training_sample)[total_sample_count - 1]).key);    
     //printf("key1 %ld, key2 %ld \n", ((*(args->rmi->training_sample))[0]).key, ((*(args->rmi->training_sample))[total_sample_count - 1]).key);
     //printf("key1 %ld, key2 %ld \n", ((*(args->rmi->training_sample))[0]).key, ((*(args->rmi->training_sample))[total_sample_count - 1]).key);        
       // Populate the training data for the root model
-      //for (unsigned int i = 0; i < total_sample_count; ++i) {
+      for (unsigned int i = 0; i < total_sample_count; ++i) {
       //  printf("i %f \n", 1. * i / total_sample_count);
         //printf("key %ld, payload %ld, value %lf \n", ((*(args->rmi->training_sample))[i]).key, ((*(args->rmi->training_sample))[i]).payload, 1. * i / total_sample_count);
-        //(*training_data)[0][0].push_back({((*(args->rmi->training_sample))[i]), 1. * i / total_sample_count});
-      //}
-
+        (*training_data)[0][0].push_back({((*(args->rmi->training_sample))[i]), 1. * i / total_sample_count});
+      }
+/*
       // Train the root model using linear interpolation
       auto *current_training_data = &(*training_data)[0][0];
       typename RMI<KeyType, PayloadType>::linear_model *current_model = &args->rmi->models[0][0];
