7ceeaab6f055b2f220166cdacf04b3d34aec23cf
Ibrahim Sabek <sabek@mit.edu>
2021-03-01 02:37:30 -0500

    Learned IMV version of non partition join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -519,11 +519,6 @@ void npj_build_rel_r_partition_learned_imv(ETHNonPartitionJoinBuild<KeyType, Pay
     auto root_slope = rmi->models[0][0].slope;
     auto root_intrcpt = rmi->models[0][0].intercept;
     unsigned int num_models = rmi->hp.arch[1];
-    /*vector<double> slopes, intercepts;
-    for (unsigned int j = 0; j < num_models; ++j) {
-        slopes.push_back(rmi->models[1][j].slope);
-        intercepts.push_back(rmi->models[1][j].intercept);
-    }*/
     vector<double>* slopes = ((ETHNonPartitionJoinBuild<KeyType, PayloadType> *)build_input)->slopes;
     vector<double>* intercepts = ((ETHNonPartitionJoinBuild<KeyType, PayloadType> *)build_input)->intercepts;
     static const unsigned int FANOUT = rmi->hp.fanout;
@@ -1276,14 +1271,13 @@ uint64_t npj_probe_rel_s_partition_learned_imv(Relation<KeyType, PayloadType> * 
     auto root_slope = rmi->models[0][0].slope;
     auto root_intrcpt = rmi->models[0][0].intercept;
     unsigned int num_models = rmi->hp.arch[1];
-    /*vector<double> slopes, intercepts;
-    for (unsigned int j = 0; j < num_models; ++j) {
-        slopes.push_back(rmi->models[1][j].slope);
-        intercepts.push_back(rmi->models[1][j].intercept);
-    }*/
     vector<double>* slopes = ((ETHNonPartitionJoinBuild<KeyType, PayloadType> *)build_output)->slopes;
     vector<double>* intercepts = ((ETHNonPartitionJoinBuild<KeyType, PayloadType> *)build_output)->intercepts;
     static const unsigned int FANOUT = rmi->hp.fanout;
+
+    int deltaT = 0; struct timeval t1, t2;
+
+    gettimeofday(&t1, NULL);
 
     int32_t k = 0, num, num_temp, done = 0, new_add = 0;
     __m512i v_base_offset_upper = _mm512_set1_epi64(rel_s_partition->num_tuples * sizeof(Tuple<KeyType, PayloadType>)), v_offset, v_base_offset,
@@ -1300,7 +1294,10 @@ uint64_t npj_probe_rel_s_partition_learned_imv(Relation<KeyType, PayloadType> * 
     __attribute__((aligned(CACHE_LINE_SIZE))) __mmask8 mask[NPJ_VECTOR_SCALE + 1], m_valid_bucket = 0, m_match = 0; //check m_to_insert = 0, m_no_conflict
     __attribute__((aligned(CACHE_LINE_SIZE))) uint64_t cur_offset = 0, base_off[NPJ_MAX_VECTOR_SCALE], *ht_pos, *slopes_pos, *intercepts_pos;
     
-    Bucket<KeyType, PayloadType> * bucket;
+    gettimeofday(&t2, NULL);
+
+    deltaT = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
+    printf("---- Initialization overhead of imv time (ms) = %10.4lf\n",  deltaT * 1.0 / 1000);
 
     for (int i = 0; i <= NPJ_VECTOR_SCALE; ++i) 
     {
