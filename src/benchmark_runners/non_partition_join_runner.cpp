caae5c5a68dd333630f061032de6d4080d419b36
Ibrahim Sabek <sabek@mit.edu>
2021-03-01 13:04:39 -0500

    Learned IMV version of non partition join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -515,20 +515,20 @@ void npj_build_rel_r_partition_learned(ETHNonPartitionJoinBuild<KeyType, Payload
 
         if(rel_r_partition->tuples[i].key < 10){
             int curr_buckts_num;
-            for(i=0; i < 2; i++)
+            for(int j=0; j < 5; j++)
             {
-                Bucket<KeyType, PayloadType> * b = ht->buckets+i;
-                if((i < 5) && b && (b->count > 0))
-                    printf("learned build i %ld key %ld \n", i, b->tuples[0].key);
+                Bucket<KeyType, PayloadType> * b = ht->buckets+j;
+                if((j < 5) && b && (b->count > 0))
+                    printf("learned build j %ld key %ld \n", j, b->tuples[0].key);
                 curr_buckts_num = 0;
                 do {
                     b = b->next;
-                    if((i < 5) && b && (b->count > 0))
-                        printf("learned build i %ld key %ld \n", i, b->tuples[0].key);
+                    if((j < 5) && b && (b->count > 0))
+                        printf("learned build j %ld key %ld \n", j, b->tuples[0].key);
                     curr_buckts_num++;
                 } while(b);
-                if((curr_buckts_num > 2) && (i < 100))
-                    printf("learned build i %ld curr_buckets_num %d nbuckets %ld FANOUT %ld \n", i, curr_buckts_num, ht->num_buckets, FANOUT);
+                if((curr_buckts_num > 2) && (j < 100))
+                    printf("learned build j %ld curr_buckets_num %d nbuckets %ld FANOUT %ld \n", j, curr_buckts_num, ht->num_buckets, FANOUT);
             }
         }
         unlock(&curr->latch);
