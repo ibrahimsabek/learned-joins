a21ebc0ac5b0f911df34c7237bc8aefd2650b4ab
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-04-11 18:04:01 -0400

    Fixing bugs for non partition join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -1998,31 +1998,31 @@ void * npj_join_thread(void * param)
     if (args->tid == 0) {
 
 #ifndef RUN_LEARNED_TECHNIQUES        
-        //strcpy(npj_pfun[1].fun_name, "IMV");
-        strcpy(npj_pfun[0].fun_name, "Naive");
+        strcpy(npj_pfun[0].fun_name, "IMV");
+        //strcpy(npj_pfun[1].fun_name, "Naive");
 
-        //npj_pfun[1].fun_ptr = npj_build_rel_r_partition_imv;
-        npj_pfun[0].fun_ptr = npj_build_rel_r_partition;
+        npj_pfun[0].fun_ptr = npj_build_rel_r_partition_imv;
+        //npj_pfun[1].fun_ptr = npj_build_rel_r_partition;
 
-        //strcpy(npj_pfun1[1].fun_name, "IMV");
-        strcpy(npj_pfun1[0].fun_name, "Naive");
+        strcpy(npj_pfun1[0].fun_name, "IMV");
+        //strcpy(npj_pfun1[1].fun_name, "Naive");
 
-        //npj_pfun1[1].fun_ptr = npj_probe_rel_s_partition_imv;
-        npj_pfun1[0].fun_ptr = npj_probe_rel_s_partition;
+        npj_pfun1[0].fun_ptr = npj_probe_rel_s_partition_imv;
+        //npj_pfun1[1].fun_ptr = npj_probe_rel_s_partition;
 
         npj_pf_num = 1;
 #else
-        strcpy(npj_pfun[0].fun_name, "Learned");
-        //strcpy(npj_pfun[1].fun_name, "Learned IMV");
+        //strcpy(npj_pfun[1].fun_name, "Learned");
+        strcpy(npj_pfun[0].fun_name, "Learned IMV");
 
-        npj_pfun[0].fun_ptr = npj_build_rel_r_partition_learned;
-        //npj_pfun[1].fun_ptr = npj_build_rel_r_partition_learned_imv;
+        //npj_pfun[1].fun_ptr = npj_build_rel_r_partition_learned;
+        npj_pfun[0].fun_ptr = npj_build_rel_r_partition_learned_imv;
 
-        strcpy(npj_pfun1[0].fun_name, "Learned");
-        //strcpy(npj_pfun1[1].fun_name, "Learned IMV");
+        //strcpy(npj_pfun1[1].fun_name, "Learned");
+        strcpy(npj_pfun1[0].fun_name, "Learned IMV");
         
-        npj_pfun1[0].fun_ptr = npj_probe_rel_s_partition_learned;
-        //npj_pfun1[1].fun_ptr = npj_probe_rel_s_partition_learned_imv;
+        //npj_pfun1[1].fun_ptr = npj_probe_rel_s_partition_learned;
+        npj_pfun1[0].fun_ptr = npj_probe_rel_s_partition_learned_imv;
 
         npj_pf_num = 1;
 #endif        
