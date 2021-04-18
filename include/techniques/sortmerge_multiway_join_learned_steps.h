d72cd14564726c3f864a2e3dc2b1a242f63f4ad3
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-04-18 14:57:05 -0400

    Fixing issues for learned_imv_sort_join

--- include/techniques/sortmerge_multiway_join_learned_steps.h
+++ include/techniques/sortmerge_multiway_join_learned_steps.h
@@ -82,6 +82,12 @@ class LearnedSortMergeMultiwayJoinSteps : public ETHSortMergeMultiwayJoinSteps<K
     if(learnedsortflag)
     {
 #ifdef USE_LEARNED_SORT_FOR_SORT_MERGE
+    Tuple<KeyType, PayloadType> * rtuples = args->threadrelchunks[args->my_tid][0].R.tuples;
+    printf("before sorting: my_tid %d R num_elems %ld min %ld %ld max %ld %ld\n", args->my_tid, args->threadrelchunks[args->my_tid][0].R.num_tuples, 
+        rtuples[0].key, rtuples[0].payload, 
+        rtuples[args->threadrelchunks[args->my_tid][0].R.num_tuples - 1].key, 
+        rtuples[args->threadrelchunks[args->my_tid][0].R.num_tuples - 1].payload);
+
         learned_sort_for_sort_merge::sort_avx(outptrR, args->rmi_r, args->NUM_MINOR_BCKT_PER_MAJOR_BCKT_r, args->MINOR_BCKTS_OFFSET_r, args->TOT_NUM_MINOR_BCKTS_r, 
                                     args->INPUT_SZ_r, inptrR, args->numR - args->tmp_total_repeatedKeysCountsR, 
                                     args->tmp_minor_bckts_r, args->tmp_minor_bckt_sizes_r,
@@ -356,7 +362,7 @@ class LearnedSortMergeMultiwayJoinSteps : public ETHSortMergeMultiwayJoinSteps<K
     Tuple<KeyType, PayloadType> * rtuples = args->threadrelchunks[args->my_tid][0].R.tuples;
     Tuple<KeyType, PayloadType> * stuples = args->threadrelchunks[args->my_tid][0].S.tuples;
 
-
+/*
     printf("my_tid %d R num_elems %ld min %ld %ld max %ld %ld\n", args->my_tid, args->threadrelchunks[args->my_tid][0].R.num_tuples, 
         rtuples[0].key, rtuples[0].payload, rtuples[args->threadrelchunks[args->my_tid][0].R.num_tuples - 1].key, rtuples[args->threadrelchunks[args->my_tid][0].R.num_tuples - 1].payload);
     for(int i = 0; i < args->threadrelchunks[args->my_tid][0].R.num_tuples - 1; i++)
@@ -371,7 +377,7 @@ class LearnedSortMergeMultiwayJoinSteps : public ETHSortMergeMultiwayJoinSteps<K
       if(rtuples[i].key > rtuples[i+1].key)
        printf("my_tid %d unsorted R elems keys %ld %ld payload %ld %ld at %d %d\n", args->my_tid, rtuples[i].key, rtuples[i+1].key, rtuples[i].payload, rtuples[i+1].payload, i, i+1);
     }
-
+*/
     nresults = merge_join(rtuples, stuples, 
                 args->threadrelchunks[args->my_tid][0].R.num_tuples, 
                 args->threadrelchunks[args->my_tid][0].S.num_tuples, chainedbuf);
