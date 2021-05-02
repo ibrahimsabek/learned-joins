bca023efc82388af84302548160f01b0a75cb927
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-02 16:27:31 -0400

    Adding scripts for benchmarking indexed nested loop joins

---
+++ scripts/evaluation/non_imv_indexed_nested_loop_join_tuner.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+process_non_imv_indexed_nested_loop_join()
+{
+    threads=(64 32) #(2 4 8 16 32 64)
+
+    dataset_folder_path=/spinning/sabek/learned_join_datasets/
+
+    r_datasets=$1
+    r_datasets_sizes=$2
+    r_datasets_file_num_partitions=$3
+    r_datasets_file_extension='"'.txt'"'
+    s_datasets=$4
+    s_datasets_sizes=$5
+    s_datasets_file_num_partitions=$6
+    s_datasets_file_extension='"'.txt'"'
+
+    output_folder_path=$7
+    mkdir $output_folder_path
+
+    run_nums=$8
+
+    load_relations_for_evaluation=$9
+    persist_relations_for_evaluation=${10}
+
+    run_inlj_with_hash_index=${11}
+    run_inlj_with_learned_index=${12}
+    run_inlj_with_csstree_index=${13}
+    run_inlj_with_art32tree_index=${14}
+
+    input_hash_table_size=${15}
+
+    if [ ${run_inlj_with_hash_index} == 1 ]
+    then
+        echo "Running INLJ with hash index ..."
+
+        for ds in ${!r_datasets[@]}
+        do
+            curr_r_dataset='"'$dataset_folder_path${r_datasets[$ds]}.txt'"'
+            curr_r_dataset_size=${r_datasets_sizes[$ds]}
+            curr_r_dataset_file_num_partitions=${r_datasets_file_num_partitions[$ds]}
+            curr_s_dataset='"'$dataset_folder_path${s_datasets[$ds]}.txt'"'
+            curr_s_dataset_size=${s_datasets_sizes[$ds]}
+            curr_s_dataset_file_num_partitions=${s_datasets_file_num_partitions[$ds]}
+
+            curr_input_hash_table_size=${input_hash_table_size[$ds]}
+
+            echo 'Joining '$curr_r_dataset' '$curr_r_dataset_size' '$curr_s_dataset' '$curr_s_dataset_size'... \n'
+            
+            for th in ${!threads[@]}
+            do
+                curr_threads=${threads[$th]}
+
+                curr_output_file=$output_folder_path'non_imv_inlj_with_hash_index_tuning_v'$((ds))'_th_'$curr_threads'_hts_'$curr_input_hash_table_size'.csv'
+
+                sh $(dirname "$0")/base_configs_maker.sh -INLJ_WITH_HASH_INDEX 1 \
+												-INLJ_WITH_LEARNED_INDEX 0 \
+ 												-INLJ_WITH_CSS_TREE_INDEX 0 \
+ 												-INLJ_WITH_ART32_TREE_INDEX 0 \
+												-PREFETCH_INLJ 0 \
+												-RUN_LEARNED_TECHNIQUES_WITH_FIRST_LEVEL_ONLY 1 \
+                                                -NUM_THREADS_FOR_EVALUATION $curr_threads \
+                                                -RELATION_R_PATH $curr_r_dataset \
+                                                -RELATION_R_FOLDER_PATH '"'$dataset_folder_path'"' \
+                                                -RELATION_R_FILE_NAME '"'${r_datasets[$ds]}'"' \
+                                                -RELATION_R_FILE_EXTENSION ${r_datasets_file_extension} \
+                                                -RELATION_R_NUM_TUPLES $curr_r_dataset_size  \
+												-RELATION_R_FILE_NUM_PARTITIONS $curr_r_dataset_file_num_partitions \
+                                                -RELATION_S_PATH  $curr_s_dataset\
+                                                -RELATION_S_FOLDER_PATH '"'$dataset_folder_path'"' \
+                                                -RELATION_S_FILE_NAME '"'${s_datasets[$ds]}'"' \
+                                                -RELATION_S_FILE_EXTENSION ${s_datasets_file_extension} \
+                                                -RELATION_S_NUM_TUPLES ${curr_s_dataset_size} \
+												-RELATION_S_FILE_NUM_PARTITIONS ${curr_s_dataset_file_num_partitions} \
+												-BENCHMARK_RESULTS_PATH '"'${curr_output_file}'"' \
+												-RUN_NUMS ${run_nums} \
+												-LOAD_RELATIONS_FOR_EVALUATION ${load_relations_for_evaluation} \
+												-PERSIST_RELATIONS_FOR_EVALUATION ${persist_relations_for_evaluation}
+
+                sh $(dirname "$0")/eth_configs_maker.sh   -BUCKET_SIZE 1 \
+                                                          -PREFETCH_DISTANCE 128 \
+                                                          -USE_MURMUR3_HASH 1 \
+                                                          -INPUT_HASH_TABLE_SIZE $curr_input_hash_table_size
+
+                cmake -DCMAKE_BUILD_TYPE=Release -DVECTORWISE_BRANCHING=on -DKNL=OFF  $(dirname "$0")/../..
+
+                cd $(dirname "$0")/../../build/release
+
+                make
+
+                ./non_imv_indexed_nested_loop_join_runner
+            done
+        done
+
+    fi
+
+												 #-INLJ_RMI_DATA_PATH '"'/spinning/sabek/rmi_data'"' \
+												 #-INLJ_RMI_NAMESPACE r_UNIQUE_v3_uint32_uint32_128000000_key_uint32_8 \
+												 #-INLJ_CSS_TREE_FANOUT 33 \
+
+}
+
+run_nums=10
+load_relations_for_evaluation=1
+persist_relations_for_evaluation=0
+
+#unique datasets
+r_unique_datasets=(r_UNIQUE_v8_uint32_uint32_1664000000) #(r_UNIQUE_v1_uint32_uint32_16000000 r_UNIQUE_v2_uint32_uint32_32000000 r_UNIQUE_v3_uint32_uint32_128000000 r_UNIQUE_v4_uint32_uint32_384000000 r_UNIQUE_v5_uint32_uint32_640000000 r_UNIQUE_v6_uint32_uint32_896000000 r_UNIQUE_v7_uint32_uint32_1152000000 r_UNIQUE_v8_uint32_uint32_1664000000 r_UNIQUE_v9_uint32_uint32_1920000000)
+s_unique_datasets=(s_UNIQUE_v8_uint32_uint32_1664000000) #(s_UNIQUE_v1_uint32_uint32_16000000 s_UNIQUE_v2_uint32_uint32_32000000 s_UNIQUE_v3_uint32_uint32_128000000 s_UNIQUE_v4_uint32_uint32_384000000 s_UNIQUE_v5_uint32_uint32_640000000 s_UNIQUE_v6_uint32_uint32_896000000 s_UNIQUE_v7_uint32_uint32_1152000000 s_UNIQUE_v8_uint32_uint32_1664000000 s_UNIQUE_v9_uint32_uint32_1920000000)
+r_unique_datasets_sizes=(1664E6) #(16E6 32E6 128E6 384E6 640E6 896E6 1152E6 1664E6 1920E6)
+s_unique_datasets_sizes=(1664E6) #(16E6 32E6 128E6 384E6 640E6 896E6 1152E6 1664E6 1920E6)
+r_unique_datasets_file_num_partitions=(64) #(64 64 64 64 64 64 64 64 64)
+s_unique_datasets_file_num_partitions=(64) #(64 64 64 64 64 64 64 64 64)
+output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_unique/
+input_hash_table_size=(1073741824) #(1073741824 ....)
+
+
+process_non_imv_indexed_nested_loop_join $r_unique_datasets $r_unique_datasets_sizes $r_unique_datasets_file_num_partitions $s_unique_datasets $s_unique_datasets_sizes $s_unique_datasets_file_num_partitions $output_folder_path $run_nums $load_relations_for_evaluation $persist_relations_for_evaluation 0 0 0 0 $input_hash_table_size
+
+
+#lognormal datasets
+#r_lognormal_datasets=(r_LOGNORMAL_v1_uint32_uint32_segma_1.000000_16000000) #(r_LOGNORMAL_v1_uint32_uint32_segma_1.000000_16000000 r_LOGNORMAL_v2_uint32_uint32_segma_1.000000_32000000 r_LOGNORMAL_v3_uint32_uint32_segma_1.000000_128000000 r_LOGNORMAL_v4_uint32_uint32_segma_1.000000_384000000 r_LOGNORMAL_v5_uint32_uint32_segma_1.000000_640000000 r_LOGNORMAL_v6_uint32_uint32_segma_1.000000_896000000 r_LOGNORMAL_v7_uint32_uint32_segma_1.000000_1152000000 r_LOGNORMAL_v8_uint32_uint32_segma_1.000000_1664000000 r_LOGNORMAL_v9_uint32_uint32_segma_1.000000_1920000000)
+#s_lognormal_datasets=(s_LOGNORMAL_v1_uint32_uint32_segma_1.000000_16000000) #(s_LOGNORMAL_v1_uint32_uint32_segma_1.000000_16000000 s_LOGNORMAL_v2_uint32_uint32_segma_1.000000_32000000 s_LOGNORMAL_v3_uint32_uint32_segma_1.000000_128000000 s_LOGNORMAL_v4_uint32_uint32_segma_1.000000_384000000 s_LOGNORMAL_v5_uint32_uint32_segma_1.000000_640000000 s_LOGNORMAL_v6_uint32_uint32_segma_1.000000_896000000 s_LOGNORMAL_v7_uint32_uint32_segma_1.000000_1152000000 s_LOGNORMAL_v8_uint32_uint32_segma_1.000000_1664000000 s_LOGNORMAL_v9_uint32_uint32_segma_1.000000_1920000000)
+#r_lognormal_datasets_sizes=(16E6) #(16E6 32E6 128E6 384E6 640E6 896E6 1152E6 1664E6 1920E6)
+#s_lognormal_datasets_sizes=(16E6) #(16E6 32E6 128E6 384E6 640E6 896E6 1152E6 1664E6 1920E6)
+#process_non_imv_indexed_nested_loop_join $r_lognormal_datasets $r_lognormal_datasets_sizes $s_lognormal_datasets $s_lognormal_datasets_sizes /spinning/sabek/learned_join_results/sort_merge_join_tuning_lognormal/
