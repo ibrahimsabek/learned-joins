b628ecb904e6c158d156563966ff15ba96e64c08
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-15 10:29:28 -0400

    Generating results and analysis

--- scripts/evaluation/non_imv_indexed_nested_loop_join_tuner.sh
+++ scripts/evaluation/non_imv_indexed_nested_loop_join_tuner.sh
@@ -312,7 +312,7 @@ s_unique_datasets_file_num_partitions=(32 32 32) #(64 64 64 64 64 64 64 64 64)
 input_hash_table_size=(16777216 16777216 16777216) #(16777216(for_16E6) 33554432(for_32E6) 134217728(for_128E6) 536870912(for_640E6) 1073741824(for_1664E6) 2147483648(for_1920E6))
 
 output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_unique/
-process_non_imv_indexed_nested_loop_join r_unique_datasets[@] $r_unique_datasets_sizes $r_unique_datasets_file_num_partitions $s_unique_datasets $s_unique_datasets_sizes $s_unique_datasets_file_num_partitions $output_folder_path $run_nums $load_relations_for_evaluation $persist_relations_for_evaluation 1 0 0 0 $input_hash_table_size
+process_non_imv_indexed_nested_loop_join ${r_unique_datasets[@]} $r_unique_datasets_sizes $r_unique_datasets_file_num_partitions $s_unique_datasets $s_unique_datasets_sizes $s_unique_datasets_file_num_partitions $output_folder_path $run_nums $load_relations_for_evaluation $persist_relations_for_evaluation 1 0 0 0 $input_hash_table_size
 #output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_learned_index_unique/
 #process_non_imv_indexed_nested_loop_join $r_unique_datasets $r_unique_datasets_sizes $r_unique_datasets_file_num_partitions $s_unique_datasets $s_unique_datasets_sizes $s_unique_datasets_file_num_partitions $output_folder_path $run_nums $load_relations_for_evaluation $persist_relations_for_evaluation 0 1 0 0 $input_hash_table_size
 #output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_csstree_index_unique/
