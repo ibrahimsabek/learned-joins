ec19ee5fc05a6b6b59bf8188e6e82a465acdfa3e
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-30 14:20:52 -0400

    Optimizing the AVX IMV implementation for non-partition join

--- src/benchmarks/tpch/queries/q3.cpp
+++ src/benchmarks/tpch/queries/q3.cpp
@@ -314,9 +314,9 @@ void printResultQ3(BlockRelation* result) {
     auto elementsInBlock = block.size();
     found += elementsInBlock;
     auto selCust = reinterpret_cast</*types::Integer*/uint32_t*>(block.data(selCustAttr));
-    //for (size_t i = 0; i < elementsInBlock; ++i) {
-    //  cout << ret[i] << "\t" << status[i] << "\t" << qty[i] << "\t" << base[i] << "\t" << disc[i] << "\t" << charge[i] << "\t" << order[i] << endl;
-    //}
+    for (size_t i = 0; i < elementsInBlock; ++i) {
+      cout << selCust[i] << endl;
+    }
   }
   cout << "total results number = " << found << endl;
 }
