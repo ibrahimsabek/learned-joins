78e6e6a3b74635f2dfeb57522b4654070b351782
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-31 12:18:07 -0400

    support for tpch benchmark

--- src/benchmarks/tpch/queries/q9.cpp
+++ src/benchmarks/tpch/queries/q9.cpp
@@ -537,7 +537,7 @@ std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
    r->rootOp = popOperator();
    return r;
 }
-*/
+
 
 std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
 
@@ -596,7 +596,8 @@ std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
    r->rootOp = popOperator();
    return r;
 }
-/*
+*/
+
 std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
 
    using namespace vectorwise;
@@ -613,7 +614,7 @@ std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
    r->rootOp = popOperator();
    return r;
 }
-
+/*
 std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
 
    using namespace vectorwise;
@@ -752,7 +753,7 @@ q9_vectorwise(runtime::Database& db, size_t nrThreads, size_t vectorSize) {
                       dynamic_cast<ResultWriter*>(query->rootOp.get())->shared.result);
     });
 
-   printResultQ9(result.get()->result.get(), "pspp");
+   printResultQ9(result.get()->result.get(), "l_suppkey");
 
   return result;
 }
