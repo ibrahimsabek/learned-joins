bf09d1e369cc17e7b4899df47f944baf049b1eac
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-31 16:37:26 -0400

    support for tpch benchmark

--- src/benchmarks/tpch/queries/q18.cpp
+++ src/benchmarks/tpch/queries/q18.cpp
@@ -407,7 +407,7 @@ std::unique_ptr<Q18Builder::Q18> Q18Builder::getQuery() {
    r->rootOp = popOperator();
    return r;
 }
-*/
+
 
 std::unique_ptr<Q18Builder::Q18> Q18Builder::getQuery() {
    using namespace vectorwise;
@@ -424,7 +424,8 @@ std::unique_ptr<Q18Builder::Q18> Q18Builder::getQuery() {
    r->rootOp = popOperator();
    return r;
 }
-/*
+*/
+
 std::unique_ptr<Q18Builder::Q18> Q18Builder::getQuery() {
    using namespace vectorwise;
 
@@ -470,7 +471,7 @@ std::unique_ptr<Q18Builder::Q18> Q18Builder::getQuery() {
    r->rootOp = popOperator();
    return r;
 }
-
+/*
 std::unique_ptr<Q18Builder::Q18> Q18Builder::getQuery() {
    using namespace vectorwise;
 
@@ -582,7 +583,7 @@ std::unique_ptr<runtime::Query> q18_vectorwise(Database& db, size_t nrThreads,
                       ->shared.result);
     });
 
-   printResultQ18(result.get()->result.get(), "c_custkey");
+   printResultQ18(result.get()->result.get(), "orders_matches");
 
 
   return result;
