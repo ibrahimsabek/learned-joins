10ec7c926d458ee7fa44715516b58fa9cbd0b538
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-31 12:48:43 -0400

    support for tpch benchmark

--- src/benchmarks/tpch/queries/q9.cpp
+++ src/benchmarks/tpch/queries/q9.cpp
@@ -476,7 +476,7 @@ std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
    r->rootOp = popOperator();
    return r;
 }
-*/
+
 
 std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
 
@@ -506,8 +506,8 @@ std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
    r->rootOp = popOperator();
    return r;
 }
+*/
 
-/*
 
 std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
 
@@ -539,6 +539,7 @@ std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
    return r;
 }
 
+/*
 
 std::unique_ptr<Q9Builder::Q9> Q9Builder::getQuery(){
 
@@ -755,7 +756,7 @@ q9_vectorwise(runtime::Database& db, size_t nrThreads, size_t vectorSize) {
                       dynamic_cast<ResultWriter*>(query->rootOp.get())->shared.result);
     });
 
-   printResultQ9(result.get()->result.get(), "nation_supplier");
+   printResultQ9(result.get()->result.get(), "part_partsupp");
 
   return result;
 }
