68debb5c34502ffe6e83a003e6e810e26198bfb0
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-05-31 01:07:18 -0400

    support for tpch benchmark

--- src/benchmarks/tpch/queries/q5.cpp
+++ src/benchmarks/tpch/queries/q5.cpp
@@ -836,7 +836,7 @@ void printResultQ5(BlockRelation* result) {
   }
   cout << "s_nationkey total results number = " << found << endl;
 
-  //materialize_one_relation<RELATION_KEY_TYPE, RELATION_PAYLOAD_TYPE>(nationKey, found);   
+  materialize_one_relation<RELATION_KEY_TYPE, RELATION_PAYLOAD_TYPE>(nationKey, found);   
 }
 
 /*
