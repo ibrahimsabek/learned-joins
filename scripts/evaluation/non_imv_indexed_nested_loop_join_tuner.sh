efea9a3593314fd6456d64a911632e3e6aedc08e
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-06-02 16:29:47 -0400

    Adding sosd datasets

--- scripts/evaluation/non_imv_indexed_nested_loop_join_tuner.sh
+++ scripts/evaluation/non_imv_indexed_nested_loop_join_tuner.sh
@@ -468,11 +468,11 @@ input_hash_table_size=(536870912) #(16777216(for_16E6) 33554432(for_32E6) 134217
 #input_hash_table_size=(536870912 1073741824 536870912) #(536870912 1073741824 536870912 1073741824 536870912) #(16777216(for_16E6) 33554432(for_32E6) 134217728(for_128E6) 536870912(for_640E6) 1073741824(for_1664E6) 2147483648(for_1920E6))
 
 
-output_folder_path=/spinning/sabek/learned_join_results/sj_with_learned_sosd_books_200M_uint32/
-#output_folder_path=/spinning/sabek/learned_join_results/sj_with_learned_sosd_books_800M_uint64/
-#output_folder_path=/spinning/sabek/learned_join_results/sj_with_learned_sosd_fb_200M_uint64/
-#output_folder_path=/spinning/sabek/learned_join_results/sj_with_learned_sosd_osm_cellids_800M_uint64/
-#output_folder_path=/spinning/sabek/learned_join_results/sj_with_learned_sosd_wiki_ts_200M_uint64/
+output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_sosd_books_200M_uint32/
+#output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_sosd_books_800M_uint64/
+#output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_sosd_fb_200M_uint64/
+#output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_sosd_osm_cellids_800M_uint64/
+#output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_hash_index_sosd_wiki_ts_200M_uint64/
 process_non_imv_indexed_nested_loop_join $r_datasets $r_datasets_sizes $r_datasets_file_num_partitions $s_datasets $s_datasets_sizes $s_datasets_file_num_partitions $output_folder_path $run_nums $load_relations_for_evaluation $persist_relations_for_evaluation 1 0 0 0 $input_hash_table_size
 
 #output_folder_path=/spinning/sabek/learned_join_results/non_imv_inlj_with_learned_index_sosd/
