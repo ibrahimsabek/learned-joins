b912faec2676210ba5ab8997845455f71d0f339c
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-06-03 03:04:28 -0400

    Adding sosd datasets

--- include/utils/data_generation.h
+++ include/utils/data_generation.h
@@ -235,7 +235,7 @@ void random_real_data_uint_gen(Relation<KeyType, PayloadType> * rel,string filen
                 rel->tuples[i].key = static_cast<KeyType>(val);//(KeyType)(val);
                 if (i < 10){
                 //printf("uint64 %lf v_int[i] %lf val %lf key %u ratio %lf \n", static_cast<double>(std::numeric_limits<uint64_t>::max()), v_int[i], val, rel->tuples[i].key, ratio);
-                printf("key %u \n", rel->tuples[i].key/*, rel->tuples[i].payload*/);
+                printf("key %d \n", rel->tuples[i].key/*, rel->tuples[i].payload*/);
                 }
             #ifdef ZERO_PAYLOAD
                 rel->tuples[i].payload = (PayloadType) 0; 
