b0cd23157894e6753e16e2cdfe95dc37c191cd16
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-06-03 02:11:54 -0400

    Adding sosd datasets

--- include/utils/data_generation.h
+++ include/utils/data_generation.h
@@ -247,7 +247,7 @@ void random_real_data_uint_gen(Relation<KeyType, PayloadType> * rel,string filen
     }
 
     /* randomly shuffle elements */
-    knuth_shuffle<KeyType, PayloadType>(rel);
+    //knuth_shuffle<KeyType, PayloadType>(rel);
 }
 
 
@@ -299,7 +299,7 @@ void random_seq_holes_gen(Relation<KeyType, PayloadType> * rel)
     }
 
     /* randomly shuffle elements */
-    //knuth_shuffle<KeyType, PayloadType>(rel);
+    knuth_shuffle<KeyType, PayloadType>(rel);
 }
 
 //based on the ETH implementation 
