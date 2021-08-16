73e8e13495d2f81550e1a74982e18aa9cb41520e
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-08-16 13:40:27 -0400

    Adding cuckoo hash index

--- include/utils/stanford_hash.h
+++ include/utils/stanford_hash.h
@@ -16,8 +16,10 @@
 #include <random>
 #include <vector>
 
+#include "utils/math.h"
+
 const uint32_t INVALID_KEY = 0xffffffff;     // An unusable key we'll treat as a sentinel value
-const uint32_t BUCKET_SIZE = 8;              // Bucket size for cuckoo hash
+const uint32_t bckt_size = 8;              // Bucket size for cuckoo hash
 const double LOAD_FACTOR = 0.99;             // Load factor used for hash tables
 
 // Finalization step of Murmur3 hash
@@ -31,9 +33,9 @@ uint32_t hash32(uint32_t value) {
 }
 
 // Fast alternative to modulo from Daniel Lemire
-uint32_t alt_mod(uint32_t x, uint32_t n) {
-  return ((uint64_t) x * (uint64_t) n) >> 32 ;
-}
+//uint32_t alt_mod(uint32_t x, uint32_t n) {
+//  return ((uint64_t) x * (uint64_t) n) >> 32 ;
+//}
 
 // A bucketed cuckoo hash map with keys of type uint32_t and values of type V
 template <typename V>
@@ -46,8 +48,8 @@ public:
 
 private:
   struct Bucket {
-    uint32_t keys[BUCKET_SIZE] __attribute__((aligned(32)));
-    V values[BUCKET_SIZE];
+    uint32_t keys[bckt_size] __attribute__((aligned(32)));
+    V values[bckt_size];
   };
 
   Bucket *buckets_;
@@ -58,11 +60,11 @@ private:
 
 public:
   CuckooHashMap(uint32_t capacity): size_(0) {
-    num_buckets_ = (capacity + BUCKET_SIZE - 1) / BUCKET_SIZE;
+    num_buckets_ = (capacity + bckt_size - 1) / bckt_size;
     int r = posix_memalign((void **) &buckets_, 32, num_buckets_ * sizeof(Bucket));
     if (r != 0) cout << "could not memalign in cuckoo hash map \n";
     for (uint32_t i = 0; i < num_buckets_; i++) {
-      for (size_t j = 0; j < BUCKET_SIZE; j++) {
+      for (size_t j = 0; j < bckt_size; j++) {
         buckets_[i].keys[j] = INVALID_KEY;
       }
     }
@@ -154,32 +156,32 @@ private:
     }
 
     size_t count1 = 0;
-    for (size_t i = 0; i < BUCKET_SIZE; i++) {
+    for (size_t i = 0; i < bckt_size; i++) {
       count1 += (b1->keys[i] != INVALID_KEY ? 1 : 0);
     }
     size_t count2 = 0;
-    for (size_t i = 0; i < BUCKET_SIZE; i++) {
+    for (size_t i = 0; i < bckt_size; i++) {
       count2 += (b2->keys[i] != INVALID_KEY ? 1 : 0);
     }
 
-    if (count1 <= count2 && count1 < BUCKET_SIZE) {
+    if (count1 <= count2 && count1 < bckt_size) {
       // Add it into bucket 1
       b1->keys[count1] = key;
       b1->values[count1] = value;
-    } else if (count2 < BUCKET_SIZE) {
+    } else if (count2 < bckt_size) {
       // Add it into bucket 2
       b2->keys[count2] = key;
       b2->values[count2] = value;
     } else {
       // Both buckets are full; evict a random item from one of them
-      assert(count1 == BUCKET_SIZE);
-      assert(count2 == BUCKET_SIZE);
+      assert(count1 == bckt_size);
+      assert(count2 == bckt_size);
 
       Bucket *victim_bucket = b1;
       if (rand_() % 2 == 0) {
         victim_bucket = b2;
       }
-      uint32_t victim_index = rand_() % BUCKET_SIZE;
+      uint32_t victim_index = rand_() % bckt_size;
       uint32_t old_key = victim_bucket->keys[victim_index];
       V old_value = victim_bucket->values[victim_index];
       victim_bucket->keys[victim_index] = key;
