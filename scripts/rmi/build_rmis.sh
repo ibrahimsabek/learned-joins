f88c58ebac4bc1989d166183640aac5a379ff07a
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-10-21 11:57:34 -0400

    Adding a script for preparing data for hashing project

--- scripts/rmi/build_rmis.sh
+++ scripts/rmi/build_rmis.sh
@@ -107,16 +107,26 @@ cd $(dirname "$0")/../../RMI && cargo build --release && cd ../build/release
 #optimize_rmis books_200M_uint32 /spinning/sabek/learned_join_datasets_sosd/books_200M_uint32 $(dirname "$0")/../../include/rmi_specs 32
 #build_rmi_set books_200M_uint32 /spinning/sabek/learned_join_datasets_sosd/books_200M_uint32 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
 
-optimize_rmis books_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/books_800M_uint64 $(dirname "$0")/../../include/rmi_specs 32
-build_rmi_set books_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/books_800M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+#optimize_rmis books_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/books_800M_uint64 $(dirname "$0")/../../include/rmi_specs 32
+#build_rmi_set books_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/books_800M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
 
-optimize_rmis fb_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/fb_200M_uint64 $(dirname "$0")/../../include/rmi_specs 32
-build_rmi_set fb_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/fb_200M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+#optimize_rmis fb_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/fb_200M_uint64 $(dirname "$0")/../../include/rmi_specs 32
+#build_rmi_set fb_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/fb_200M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
 
-optimize_rmis osm_cellids_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/osm_cellids_800M_uint64 $(dirname "$0")/../../include/rmi_specs 32
-build_rmi_set osm_cellids_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/osm_cellids_800M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+#optimize_rmis osm_cellids_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/osm_cellids_800M_uint64 $(dirname "$0")/../../include/rmi_specs 32
+#build_rmi_set osm_cellids_800M_uint64 /spinning/sabek/learned_join_datasets_sosd/osm_cellids_800M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
 
-optimize_rmis wiki_ts_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/wiki_ts_200M_uint64 $(dirname "$0")/../../include/rmi_specs 32
-build_rmi_set wiki_ts_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/wiki_ts_200M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+#optimize_rmis wiki_ts_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/wiki_ts_200M_uint64 $(dirname "$0")/../../include/rmi_specs 32
+#build_rmi_set wiki_ts_200M_uint64 /spinning/sabek/learned_join_datasets_sosd/wiki_ts_200M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+
+#### the following commands are for the learned hashing project ###
+optimize_rmis consecutive_100M_uint64 /spinning/sabek/learned_hashing_datasets/consecutive_100M_uint64 $(dirname "$0")/../../include/rmi_specs 32
+build_rmi_set consecutive_100M_uint64 /spinning/sabek/learned_hashing_datasets/consecutive_100M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+
+optimize_rmis consecutive_200M_uint64 /spinning/sabek/learned_hashing_datasets/consecutive_200M_uint64 $(dirname "$0")/../../include/rmi_specs 32
+build_rmi_set consecutive_200M_uint64 /spinning/sabek/learned_hashing_datasets/consecutive_200M_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
+
+optimize_rmis consecutive_90437011_uint64 /spinning/sabek/learned_hashing_datasets/consecutive_90437011_uint64 $(dirname "$0")/../../include/rmi_specs 32
+build_rmi_set consecutive_90437011_uint64 /spinning/sabek/learned_hashing_datasets/consecutive_90437011_uint64 $(dirname "$0")/../../include/rmi $(dirname "$0")/../../include/rmi_specs /spinning/sabek/rmi_data 32
 
 /bin/bash $(dirname "$0")/generate_all_rmis.sh
