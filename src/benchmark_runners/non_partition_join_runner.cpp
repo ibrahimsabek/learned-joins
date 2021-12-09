82a5a773f2c26e95ff5722bccdc5e65db7f1c302
ibrahimsabek <ibrahim.sabek@gmail.com>
2021-12-08 23:18:58 -0500

    Benchmarking non_partition_hash_join

--- src/benchmark_runners/non_partition_join_runner.cpp
+++ src/benchmark_runners/non_partition_join_runner.cpp
@@ -2017,19 +2017,19 @@ void * npj_join_thread(void * param)
     if (args->tid == 0) {
 
 #ifndef RUN_LEARNED_TECHNIQUES        
-        strcpy(npj_pfun[1].fun_name, "IMV");
-        strcpy(npj_pfun[0].fun_name, "Naive");
+        strcpy(npj_pfun[0].fun_name, "IMV");
+        strcpy(npj_pfun[1].fun_name, "Naive");
 
-        npj_pfun[1].fun_ptr = npj_build_rel_r_partition_imv;
-        npj_pfun[0].fun_ptr = npj_build_rel_r_partition;
+        npj_pfun[0].fun_ptr = npj_build_rel_r_partition_imv;
+        npj_pfun[1].fun_ptr = npj_build_rel_r_partition;
 
-        strcpy(npj_pfun1[1].fun_name, "IMV");
-        strcpy(npj_pfun1[0].fun_name, "Naive");
+        strcpy(npj_pfun1[0].fun_name, "IMV");
+        strcpy(npj_pfun1[1].fun_name, "Naive");
 
-        npj_pfun1[1].fun_ptr = npj_probe_rel_s_partition_imv;
-        npj_pfun1[0].fun_ptr = npj_probe_rel_s_partition;
+        npj_pfun1[0].fun_ptr = npj_probe_rel_s_partition_imv;
+        npj_pfun1[1].fun_ptr = npj_probe_rel_s_partition;
 
-        npj_pf_num = 1;
+        npj_pf_num = 2;
 #else
         strcpy(npj_pfun[1].fun_name, "Learned");
         strcpy(npj_pfun[0].fun_name, "Learned IMV");
