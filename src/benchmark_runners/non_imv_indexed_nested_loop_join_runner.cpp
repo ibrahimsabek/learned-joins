245e18b41a6d5b7d5479c5bc4c90459e639d9d27
ibrahimsabek <ibrahim.sabek@gmail.com>
2022-02-24 19:08:30 -0500

    changes for supporting perfect hashing and probing techniques

--- src/benchmark_runners/non_imv_indexed_nested_loop_join_runner.cpp
+++ src/benchmark_runners/non_imv_indexed_nested_loop_join_runner.cpp
@@ -231,9 +231,11 @@ uint64_t inlj_with_hash_probe_rel_s_partition(Relation<KeyType, PayloadType> * r
                 auto it = ht->operator[](searched);  
                 matches += (keyForSearch == it.key())? 1:0;
 
-            if (i % 100000 == 0)
+            //if (i % 100000 == 0)
+            if (i > 2900000 == 0){
+                if (i % 1000 == 0)
                     printf("table size in bytes %ld here inside looping %ld key %ld found %ld with payload %ld  matches %ld\n", ht->directory_byte_size(), i, keyForSearch, it.key(), it.payload(), matches);
-
+            }
             //}
             //catch (std::exception& e)
             //{
