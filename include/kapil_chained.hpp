eecdfe7428779d6b1ec38f65dc293a12608c6a74
ibrahimsabek <ibrahim.sabek@gmail.com>
2022-03-24 15:11:44 -0400

    changes for joins in hash benchmark

--- include/kapil_chained.hpp
+++ include/kapil_chained.hpp
@@ -38,15 +38,15 @@ class KapilChainedHashTable {
     void insert(const Key& key, const Payload& payload,
                 learned_imv_joins::support::Tape<Bucket>& tape) {
       Bucket* previous = this;
-        std::cout<<"Inside bucket insert 0 key: "<< key << td::endl;     
+        std::cout<<"Inside bucket insert 0 key: "<< key << std::endl;     
 
       for (Bucket* current = previous; current != nullptr;
            current = current->next) {
-        std::cout<<"Inside bucket insert 1 key: "<< key << td::endl;     
+        std::cout<<"Inside bucket insert 1 key: "<< key << std::endl;     
         for (size_t i = 0; i < BucketSize; i++) {
-        std::cout<<"Inside bucket insert 2 key: "<< key << td::endl;       
+        std::cout<<"Inside bucket insert 2 key: "<< key << std::endl;       
           if (current->keys[i] == Sentinel) {
-                  std::cout<<"Inside bucket insert 3 key: "<< key << td::endl;       
+                  std::cout<<"Inside bucket insert 3 key: "<< key << std::endl;       
             current->keys[i] = key;
             current->payloads[i] = payload;
             return;
